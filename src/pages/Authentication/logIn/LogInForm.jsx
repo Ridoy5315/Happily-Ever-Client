f16d78b1c06bfba27729acf8269422066af9ffa8
Ridoy5315 <ridoy.st99@gmail.com>
2025-02-08 13:45:24 +0800

    captcha functionality has been added in the log in page

--- src/pages/Authentication/logIn/LogInForm.jsx
+++ src/pages/Authentication/logIn/LogInForm.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import cartonPhoto from "../../../assets/marriedSignUp.jpg";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  LoadCanvasTemplateNoReload,
+  validateCaptcha,
+} from "react-simple-captcha";
 const LogInForm = () => {
-     return (
-          <div className="grid grid-cols-2 w-10/12 mx-auto mt-12 mb-28 bg-white shadow-lg rounded-xl">
+     const captchaRef = useRef(null);
+     const [isCaptchaValid, setIsCaptchaValid] = useState(false);
+     useEffect(() => {
+          loadCaptchaEnginge(6);
+     }, [])
+
+     const handleValidateCaptcha = () => {
+          const user_captcha_value = captchaRef.current.value;
+          if(validateCaptcha(user_captcha_value)){
+               setIsCaptchaValid(true)
+          }
+          else{
+               setIsCaptchaValid(false)
+          }
+     }
+  return (
+    <div className="grid grid-cols-2 w-10/12 mx-auto mt-12 mb-28 bg-white shadow-lg rounded-xl">
       <div className="bg-gold2-color px-16 py-12 space-y-10 rounded-l-xl">
         <div className="text-maroon-color">
           <h1 className="text-5xl mb-8">Now</h1>
           <h1 className="text-6xl font-semibold leading-tight">
-          Log In to Discover Your Soulmate
+            Log In to Discover Your Soulmate
           </h1>
         </div>
         <div className="relative  mx-auto w-96 h-96 rounded-full">
@@ -26,16 +47,15 @@ const LogInForm = () => {
             Log in to your account
           </h2>
           <p className=" text-sm/6 text-gray-500">
-          Don’t have an account?{" "}
+            Don’t have an account?{" "}
             <Link className="font-semibold text-indigo-600 hover:text-indigo-500">
-            Sign Up Now
+              Sign Up Now
             </Link>
           </p>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form action="#" method="POST" className="space-y-6">
-            
             <div>
               <label className="block text-sm/6 font-medium text-gray-900">
                 Email address
@@ -50,7 +70,6 @@ const LogInForm = () => {
                 />
               </div>
             </div>
-            
 
             <div>
               <label className="block text-sm/6 font-medium text-gray-900">
@@ -66,24 +85,48 @@ const LogInForm = () => {
                   placeholder="******"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
+                <div className="text-sm mt-1">
+                  <Link className=" font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </Link>
+                </div>
+              </div>
+            </div>
+            {/* Captcha */}
+            <div>
+              <label className="block text-sm/6 font-medium text-gray-900">
+              <LoadCanvasTemplate />
+              </label>
+
+              <div className="mt-2">
+                <input
+
+                  name="captcha"
+                  type="text"
+                  ref={captchaRef}
+                  required
+                  placeholder="type the captcha above"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+                <button onClick={handleValidateCaptcha}>Validate</button>
                 
               </div>
             </div>
-            
 
             <div>
-              <button
+              <button 
+              disabled={!isCaptchaValid}
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className={`flex w-full justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${isCaptchaValid ? 'bg-indigo-600 hover:bg-indigo-500' : 'bg-gray-400 hover:bg-gray-500 cursor-not-allowed'}`}
               >
-                Sign Up
+                Log In
               </button>
             </div>
           </form>
         </div>
       </div>
     </div>
-     );
+  );
 };
 
 export default LogInForm;
