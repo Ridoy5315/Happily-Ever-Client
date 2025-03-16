bec21171024111bdf3d00dfd9b970b1bdfea076d
Ridoy5315 <ridoy.st99@gmail.com>
2025-03-17 00:29:13 +0800

    added three section in about us page

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -3,7 +3,7 @@ import banner from "../../assets/aboutUs/Ethnic-Punjabi-wedding-couple-photograp
 import SectionTitle from "../../componenets/shared/SectionTitle/SectionTitle";
 const AboutUs = () => {
   return (
-    <div>
+    <div className="mb-24">
       <div className="overflow-hidden w-full relative  lg:h-[550px] md:h-[400px] h-[350px]">
         <div className="absolute z-10 text-gray-300 px-16 md:px-52 lg:px-80 top-[35%] transform -translate-y-1/2">
           <h1 className="lg:text-5xl md:text-2xl text-lg text-center lg:leading-snug leading-normal font-medium">
@@ -43,11 +43,32 @@ const AboutUs = () => {
       </div>
       <div className="pt-24 lg:w-6/12 w-11/12 mx-auto text-gray-600">
         <h3 className="text-4xl font-semibold text-gold-color text-center">Why Choose Us?</h3>
-        <div className="mt-8 space-y-6">
-          <p className="">
-               <span>✓ Privacy & Security - </span> Your data is safe with us.
+        <div className="mt-8 space-y-4">
+          <p className="text-lg">
+               <span className="text-maroon-color text-xl font-medium">✓ Privacy & Security - </span> Your data is safe with us.
+          </p>
+          <p className="text-lg">
+               <span className="text-maroon-color text-xl font-medium">✓ User-Friendly Dashboard - </span> Simple and easy-to-use interface.
+          </p>
+          <p className="text-lg">
+               <span className="text-maroon-color text-xl font-medium">✓ Contact Privacy Control - </span> Your contact details remain hidden Admin approve the contact request.
+          </p>
+          <p className="text-lg">
+               <span className="text-maroon-color text-xl font-medium">✓ Free & Premium Membership Plans - </span> Choose from free or premium plans based on your needs. Premium members get advanced contact access.
+          </p>
+          <p className="text-lg">
+               <span className="text-maroon-color text-xl font-medium">✓ Success Stories & Happy Couples - </span> Read real success stories from couples who found love through our platform.
+          </p>
+          <p className="text-lg">
+               <span className="text-maroon-color text-xl font-medium">✓ Mobile-Friendly & Easy Access - </span> Access our platform on any device—desktop, tablet, or mobile—anytime, anywhere.
           </p>
         </div>
+
+      </div>
+      <div className="pt-24 lg:w-6/12 w-11/12 mx-auto text-gray-600">
+        <h3 className="text-4xl font-semibold text-gold-color text-center">Our Journey</h3>
+        <p className="mt-8 text-center bg-[#f5e7e4] py-5 px-6 rounded-lg">Our journey began with the vision to make online matchmaking safer, easier, and more personalized. The idea for Happily Ever was born out of a need for a safe, trustworthy, and effective matchmaking platform. Many people struggled with unreliable matchmaking services, so we decided to create a solution that prioritizes authenticity, privacy, and meaningful connections.</p>
+        
 
       </div>
     </div>
