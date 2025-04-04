b9abb80faa36ac35f24cf4b28e7cab843fa509b9
Ridoy5315 <ridoy.st99@gmail.com>
2025-04-04 23:22:40 +0800

    animation added at title and subtitle of homepage

--- src/componenets/home/premiumCards/PremiumCards.jsx
+++ src/componenets/home/premiumCards/PremiumCards.jsx
@@ -36,11 +36,6 @@ const PremiumCards = () => {
                 opacity: 1,
                 y: 0,
               }}
-              // animate={{
-              //   opacity: 1,
-              //   translateX: 0,
-              //   translateY: 0,
-              // }}
               transition={{ duration: 0.5, delay: (i % 3) * 0.2 }}
               viewport={{once: true, amount: 0.2}}
             >
--- src/componenets/shared/SectionTitle/SectionTitle.jsx
+++ src/componenets/shared/SectionTitle/SectionTitle.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { Slide } from 'react-awesome-reveal';
 const SectionTitle = ({heading, subHeading})  => {
      return (
           <div className='lg:w-6/12 md:w-7/12 w-9/12 mx-auto text-center lg:space-y-2 md:space-y-2'>
+               <Slide direction="up" duration={1500} className="overflow-hidden">
                <p className='text-gold-color lg:font-bold md:font-bold font-medium uppercase font-fontBody lg:text-lg md:text-sm text-xs'>{subHeading}</p>
-               <h3 className='lg:text-5xl md:text-3xl text-2xl text-maroon-color font-semibold font-fontHeading'>{heading}</h3>
+               <h3 className='animate__animated animate__slideInUp lg:text-5xl md:text-3xl text-2xl text-maroon-color font-semibold font-fontHeading'>{heading}</h3>
+               </Slide>
           </div>
      );
 };
